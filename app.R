ba09fd054e9a9acfcc58e8a988803e93bf017ab7
plague1981 <42053196+plague1981@users.noreply.github.com>
2024-02-09 16:57:08 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -1,10 +1,10 @@
 library(shiny)
 library(shinythemes)
 library(shinydashboard)
-
+library(ggplot2)
+source('word_questions.R')
 ui <- dashboardPage(
-  
-  # App title ----
+
   dashboardHeader(title = "Math exercise!"),
   
   dashboardSidebar(  
@@ -20,27 +20,53 @@ ui <- dashboardPage(
               navbarPage(title = 'PRES',
                          tabPanel('1st', icon = icon('dice-one'),
                                   box(width = 10
-                                    
+                                      
                                   )
                          ),
                          tabPanel('2nd', icon = icon('dice-two'),
+                                  box(width = 10, title = 'Environment settings',
+                                      selectInput(inputId = 'add_digits', label = 'How many digits?', choices = c(1,2,3), selected = 1),
+                                      actionButton(inputId = 'set', label = 'set',class='btn')
+                                  ),
                                   box(width = 10, title = 'Addition',
-                                      selectInput(inputId = 'digits', label = 'How many digits?', choices = c(1,2,3), selected = 1),
-                                      actionButton(inputId = 'generate', label = 'Generate',class='btn'),
-                                      textOutput(outputId = 'fst_number'),
-                                      textOutput(outputId = 'sec_number'),
-                                      textInput(inputId = 'add_ans'),
-                                      actionButton(inputId = 'add_submit', label = 'Generate',class='btn'),
+                                      uiOutput(outputId = 'add_equation'),
+                                      textInput(inputId = 'add_ans',label = 'Answer'),
+                                      actionButton(inputId = 'add_submit', label = 'Submit',class='btn'),
                                       textOutput(outputId = 'add_reply')
                                   ),
-                                  box(width = 10, title = 'Substraction'
-                                      
+                                  box(width = 10, title = 'Substraction',
+                                      uiOutput(outputId = 'sub_equation'),
+                                      textInput(inputId = 'sub_ans',label = 'Answer'),
+                                      actionButton(inputId = 'sub_submit', label = 'Submit',class='btn'),
+                                      textOutput(outputId = 'sub_reply')
                                   ),
-                                  box(width = 10, title = 'Multiplying'
-                                      
+                                  box(width = 10, title = 'Multiplying',
+                                      uiOutput(outputId = 'mul_equation'),
+                                      textInput(inputId = 'mul_ans',label = 'Answer'),
+                                      actionButton(inputId = 'mul_submit', label = 'Submit',class='btn'),
+                                      textOutput(outputId = 'mul_reply')
                                   ),
-                                  box(width = 10, title = 'Dividing'
-                                      
+                                  box(width = 10, title = 'Dividing',
+                                      uiOutput(outputId = 'div_equation'),
+                                      textInput(inputId = 'div_ans',label = 'Answer'),
+                                      actionButton(inputId = 'div_submit', label = 'Submit',class='btn'),
+                                      textOutput(outputId = 'div_reply')
+                                  ),
+                                  box(width = 10, title = 'Fraction',
+                                      plotOutput(outputId = 'fra_piechart'),
+                                      uiOutput(outputId = 'fra_equation'),
+                                      actionButton(inputId = 'fra_submit', label = 'Submit',class='btn'),
+                                      textOutput(outputId = 'fra_reply')
+                                  ),
+                                  box(width = 10, title = 'Word questions',
+                                      uiOutput(outputId = 'w_q_a'),
+                                      textInput(inputId = 'w_q_a_ans',label = 'Answer'),
+                                      actionButton(inputId = 'w_q_a_submit', label = 'Submit',class='btn'),
+                                      textOutput(outputId = 'w_q_a_reply'),
+                                      uiOutput(outputId = 'w_q_c'),
+                                      textInput(inputId = 'w_q_c_ans',label = 'Answer'),
+                                      actionButton(inputId = 'w_q_c_submit', label = 'Submit',class='btn'),
+                                      textOutput(outputId = 'w_q_c_reply')
                                   )
                          ),
                          tabPanel('3rd', icon = icon('dice-three'),
@@ -55,59 +81,150 @@ ui <- dashboardPage(
                          ),
                          tabPanel('5th', icon = icon('dice-five'),
                                   box(width = 10
-                                      
                                   )
                          )
               )
       ),
       tabItem(tabName = "Middle"
-        
+              
       ),
       tabItem(tabName = "High"
-        
+              
       )
     )
- 
+    
   )
 )
 
 server <- function(input, output,session) {
-  output$fst_number <- eventReactive(input$generate,{
-    sample(1:100,1)
+  numbers<- eventReactive(input$set,{
+    if (input$add_digits==1){
+      fst<-sample(0:9,1)
+      sec<-sample(0:9,1)
+      trd<-sample(0:9,1)
+    } else if (input$add_digits==2){
+      fst<-sample(0:99,1)
+      sec<-sample(0:99,1)
+      trd<-sample(0:99,1)
+    } else {
+      fst<-sample(0:999,1)
+      sec<-sample(0:999,1)
+      trd<-sample(0:999,1)
+    }
+    c(fst,sec,trd)
   })
-  output$sec_number <- eventReactive(input$generate,{
-    sample(1:100,1)
+  coins<- eventReactive(input$set,{
+    one_dollar<- sample(0:9,1)
+    quarter<- sample(0:9,1)
+    dime<- sample(0:9,1)
+    nickel<- sample(0:9,1)
+    cent<- sample(0:9,1)
+    c(one_dollar, quarter, dime, nickel, cent)
   })
-
-  
-}
-
-
-
-addition <- function(){
-  correct <- 0
-  test<- 0
-  test_total <- 2
-
-  repeat{
-    a <- sample(0:100,1)
-    b <- sample(0:100,1)
-  
-    ans <-  readline(paste0("What is the answer of", a, '+' ,b, "="))
-    if (a+b == ans){
-        correct <- correct + 1
-        print ('That is correct')
-    } else print ('That is wrong')
-
-    test <- test +1 
-    print(paste('You have finished',test,'questions.'))
-    print(paste0(correct,'/',test_total,' are right'))
+# Addition
+  output$add_equation<-renderUI({
+    return(paste(numbers()[1],'+',numbers()[2],'=?'))
+  })
+  output$add_reply<-eventReactive(input$add_submit,{
+    if(input$add_ans==(numbers()[1]+numbers()[2])){
+      return("That is correct!!!")
+    } else return("Sorry! Please try again")
+  })
+# Substraction
+  output$sub_equation<-renderUI({
+    if (numbers()[1] >= numbers()[2]){
+      return(paste(numbers()[1],'-',numbers()[2],'=?'))
+    } else return(paste(numbers()[2],'-',numbers()[1],'=?'))
     
-    if (test == test_total){
-      print(paste('Well done! Your score is', correct/test_total*100))
-      break
-    } 
-  }
+  })
+  output$sub_reply<-eventReactive(input$sub_submit,{
+    if(input$sub_ans==(abs(numbers()[1]-numbers()[2]))){
+      return("That is correct!!!")
+    } else return("Sorry! Please try again")
+  })
+# Multiply
+  output$mul_equation<-renderUI({
+    return(paste(numbers()[1],'x',numbers()[2],'=?'))
+    
+  })
+  output$mul_reply<-eventReactive(input$mul_submit,{
+    if(input$mul_ans==(numbers()[1]*numbers()[2])){
+      return("That is correct!!!")
+    } else return("Sorry! Please try again")
+  })
+# Dividing (positive integer only)
+  
+  output$div_equation<-renderUI({
+    denominator <- numbers()[1]
+    numerator <- denominator*numbers()[2]
+    return(paste0(numerator,'/',denominator,'=?'))
+  })
+  output$div_reply<-eventReactive(input$div_submit,{
+    
+    if(input$div_ans==numbers()[2]){
+      return("That is correct!!!")
+    } else return("Sorry! Please try again")
+  })
+  
+# Fraction
+  output$fra_equation<-renderUI({
+    if (numbers()[1] >= numbers()[2]){
+      ans <- paste0(numbers()[2],'/',numbers()[1])
+    } else ans <- paste0(numbers()[1],'/',numbers()[2])
+    
+    n <- 0
+    ans_w<-NULL
+    repeat {
+      n <- n + 1
+      a <- sort(sample(1:10,2),decreasing = TRUE)
+      b <- paste0(a[2],'/',a[1])
+      ans_w<-c(ans_w,b)
+      if (n == 2){
+        ans_s <-sample(c(ans_w, ans), 3 ,replace = FALSE)
+        if (anyDuplicated(ans_s)){
+          next
+        } else break
+      }
+    }
+    checkboxGroupInput(inputId = "fra_equation", inline = TRUE, label = "Please select one to represent A!", choices = ans_s, selected = NULL)
+  })
+  
+  output$fra_piechart <- renderPlot({
+    data <- data.frame(
+      group=LETTERS[1:(abs(numbers()[1]-numbers()[2])+1)],
+      value=c(min(c(numbers()[1],numbers()[2])),rep(1,abs(numbers()[1]-numbers()[2])))
+    )
+    
+    # Basic piechart
+    ggplot(data, aes(x="", y=value, fill=group)) +
+      geom_bar(stat="identity", width=1) +
+      coord_polar("y", start=0)
+  })
+  output$fra_reply <- eventReactive(input$fra_submit,{
+    if (length(input$fra_equation)>1){
+      return("Please select one answer!")
+    } else if(input$fra_equation %in% c(paste0(numbers()[1],'/',numbers()[2]),paste0(numbers()[2],'/',numbers()[1]))){
+      return("That is correct!!!")
+    } else return("Sorry! Please try again")
+  })
+  output$w_q_a<-renderUI({
+    word_q_add(numbers()[1],numbers()[2],numbers()[3])
+  })
+  output$w_q_c<-renderUI({
+    q<-word_q_coin(coins()[1],coins()[2],coins()[3],coins()[4],coins()[5])
+    return(q[[1]])
+           
+    #word_q_coin(dollar=coins()[1], quarter=coins()[2], dime=coins()[3], nickel=coins()[4], cent=coins()[5])
+  })
+  output$w_q_c_reply <- eventReactive(input$w_q_c_submit,{
+    
+    if (input$w_q_c_ans==word_q_coin(coins()[1],coins()[2],coins()[3],coins()[4],coins()[5])[[2]]){
+      return("That is correct!!!")
+    } else return("Sorry! Please try again")
+  })
 }
+
+
 
 shinyApp(ui, server)
+
---
+++ word_questions.R
@@ -0,0 +1,32 @@
+# ========= 2nd grader
+# Addition and Subtraction
+
+word_q_add<- function(a=NULL, b=NULL, c=NULL){
+  question<-c(
+    paste('Their plastic Christmas tree comes in 3 parts. The bottom part has', a, 'branches. The middle part has', b, 'branches, and the top part has', c, 'branches. How many branches are there in total?')
+    ,paste('There were some marshmallows in th bag. Jack ate', a,'. David ate', b,'. There are', c, 'marshmallows left. How many marshmallows were in the bag before Jac and David ate some?')
+      )
+  return(question[sample(1:length(question),1)])
+}
+
+word_q_coin<- function(dollar=NULL, quarter=NULL, dime=NULL, nickel=NULL, cent=NULL){
+  questions <-c(list(
+    paste('Lucas has', dollar, 'one-dollar bills,', dime, 'dimes and', nickel ,'nickels. How much money does Lucas have?'),
+    dollar+0.1*dime+0.05*nickel))
+  return(questions)
+}
+#, quarter, 'quarters,'
+word_q_sub<- function(a=NULL, b=NULL, c=NULL){
+  questions<-c(
+    paste('Jemima Puddle-Duck had', a, '. She bought a bag of corn for', b, '.How much money did she have left?'),
+    paste('Mr. Martin made', a, 'cups of macaroni and cheese for the school lunch. There were', b, 'cups left after lunch.How many cups of macaroni and cheese were eaten?')
+  )
+  return(questions[sample(1:length(questions),1)])
+}
+
+a<-function(){
+  return(list('a','b'))
+}
+
+
+
